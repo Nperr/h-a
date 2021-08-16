429e642582d8a3cc0aca52ae94f3fac0b89277a5
tang-crush-main <leotranngunguoi128@gmail.com>
2021-08-16 21:57:30 +0700

    Dan choi

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Xin chào dân chơi !',
+    introDesc: ' Tôi có điều này muốn hỏi bạn, nhớ trả lời thật lòng nha',
+    btnIntro: 'Chơii',
+    title: 'Bạn còn yêu Phương có phải không ?',
+    desc: 'Bạn không trả lời hoặc thoát ra, tức là đúng đó nha :> ',
+    btnYes: 'yêu ơi là iuuu <33',
+    btnNo: 'bạn có vấn đề về não à :333',
+    question:'Tôi biết mà, nói lí do bạn ơi :))',
+    btnReply: 'Gửi cho tôi đi',
+    reply: 'Tại vì tôi lụy U_U',
+    mess: 'Tôi biết mà !. Công khai đi bạn',
+    messDesc: 'Còn giờ thì chờ gì nữa mà không nói cho tôi đi nào.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100064206967687' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
